27aaf157aadd57cc5fe8bd0618dd72e18dd9788a
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-07-25 13:17:30 +0000

    added: auth

---
+++ app/api/auth/[...nextauth]/auth-option.ts
@@ -0,0 +1,13 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+
+export const authOption: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        })
+    ],
+
+    secret: process.env.NEXTAUTH_SECRET
+}
