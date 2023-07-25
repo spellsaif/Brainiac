07a0b37a9abdc0d9e2e1de7261ddc66e9c0dc445
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-07-25 14:26:01 +0000

    fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import {signIn} from 'next-auth/react'
+import { signIn, signOut, useSession} from 'next-auth/react'
 
 export default function Home() {
+  const session = useSession();
   return (
     <div>
-      <button onClick={()=> signIn()}>Sign In</button>
+     {session.data?.user ? (
+      <div>
+        <h1>Hello, {session.data.user.name}</h1>
+        <button onClick = {() => signOut()}>logout</button>
+      </div>
+     ):(
+      <button onClick = {() => signIn()}>Sign In</button>
+     )}
     </div>
   )
 }
